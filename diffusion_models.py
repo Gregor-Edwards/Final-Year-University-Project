Adjusted code to try and improve the class conditioning of the model when using a shorter hop length. Also fixed an issue where the generated samples were extremely quiet by normalising the audio tensor before saving it.

diffusion_models.py
@@ -362,7 +362,9 @@ class ClassConditionedAudioUnet2D(AudioUnet2D):
         self.combined_mlp = nn.Sequential(
             nn.Linear(combined_size, temb_channels),  # Reduce to match embedding size
             nn.GELU(),
+            nn.Dropout(p=0.2),  # Dropout for regularisation
             nn.Linear(temb_channels, temb_channels),  # Final projection
+            nn.GELU()  # Stabilisation
         )
 
         #self.layer_norm = nn.LayerNorm(temb_channels) # Used to normalise each embedding so that one embedding does not dominate over the others
@@ -389,6 +391,9 @@ class ClassConditionedAudioUnet2D(AudioUnet2D):
         #temb = self.layer_norm(temb)
         #class_emb = self.layer_norm(class_emb)
         #positional_emb = self.layer_norm(positional_emb)
+        # Normalize timestep and class embeddings separately
+        temb = nn.functional.normalize(temb, p=2, dim=-1)  # L2 normalization
+        class_emb = nn.functional.normalize(class_emb, p=2, dim=-1)
         
         # Combine timestep, class, and positional embeddings
         combined_emb = torch.cat([temb, class_emb], dim=-1) # torch.cat([temb, class_emb, positional_emb], dim=-1)
@@ -463,5 +468,5 @@ class ClassConditionedAudioUnet2D(AudioUnet2D):
         x = self.up2_block2(x, temb)#, class_emb)
 
         prediction = self.final_conv(x)
-        
+
         return prediction

generate_from_pretrained_model.py
@@ -88,10 +88,11 @@ def save_samples(images, folder_path, batch_name="TEST_IMAGE", save_audio=False)
             image = Image.open(generated_spectrogram_file)
             audio = dataset.mel_spectrogram_to_audio(image)
             audio_tensor = torch.tensor(audio).unsqueeze(0) # Add channel dimension due to the mono audio output (duplicate the channel so that the audio can be played)
+            reconstructed_audio = audio_tensor / torch.max(torch.abs(reconstructed_audio)) # Normalise the audio so that the volume matches normal levels and is not so quiet
 
             # Save the audio file
             output_audio_path = generated_spectrogram_file.replace(".png", ".wav")
-            torchaudio.save(output_audio_path, audio_tensor, dataset.sample_rate)
+            torchaudio.save(output_audio_path, reconstructed_audio, dataset.sample_rate)
             print(f"Audio {idx} saved!")
 
 
@@ -115,8 +116,8 @@ if __name__ == '__main__':
     model = diffusion_models.ClassConditionedAudioUnet2D(sample_size=resolution, num_classes=num_classes).to(device)#, max_position=max_slice_position).to(device)
     dataset = datasets.GTZANDataset(resolution=resolution, root_dir="GTZAN_Genre_Collection/genres", spectrogram_dir="GTZAN_Genre_Collection/slices")
     # TODO: Make sure that the hop-length, n_iter parameters are set correctly for the given pre-trained model
-    dataset.hop_length = 1024 #512
-    dataset.n_iter = 32 #100
+    dataset.hop_length = 512 #1024
+    dataset.n_iter = 100 #32
 
 
     # Setup directory to store the model parameters